3606955c57c45cc14bd52561e4b1f337b2ad3f06
Laurence May <laurence.e.may@gmail.com>
2015-04-13 21:21:02 +0100

    graphics fixed, trancparancy working

--- AsteroidsTest.py
+++ AsteroidsTest.py
@@ -1,5 +1,5 @@
 import sys, pygame, math, random
-#~~~~~~~~asteroid game~~~~~~~~#
+# ~~~~~~~~asteroid game~~~~~~~~#
 def start_state():
     size = width, height = 700, 700
     game = {
@@ -8,8 +8,8 @@ def start_state():
             "angle": 0,
             "image": pygame.image.load("space.png"),
             "rect": pygame.image.load("space.png").get_rect(),
-            "health":5,
-            "hits":0
+            "health": 5,
+            "hits": 0
         },
         "screen": pygame.display.set_mode(size),
         "background": pygame.image.load("AndromedaGalaxy.png"),
@@ -20,14 +20,14 @@ def start_state():
              "rect": pygame.image.load("asteroid.png").get_rect()
             },
         ],
-        "bullet":[{
-            "rect":pygame.image.load("bullet.png").get_rect(),
-            "image": pygame.image.load("bullet.png"),
-            "pos": {"x": 350, "y": 350},
-            "angle": 0,
-            "fired": False
+        "bullet": [{
+                       "rect": pygame.image.load("bullet.png").get_rect(),
+                       "image": pygame.image.load("bullet.png"),
+                       "pos": {"x": 350, "y": 350},
+                       "angle": 0,
+                       "fired": False
 
-            }],
+                   }],
 
     }
     return game
@@ -51,42 +51,48 @@ def init(game):
 
 
 def gameLoop(game):
-    bullet = game["bullet"][0]
     num = random.randint(0, 50)
     if num == 1:
         newAsteroid = {
-                "image": pygame.image.load("asteroid.png"),
-                "pos": {"x": 0, "y": 0},
-                "angle": random.randint(0, 90)
+            "image": pygame.image.load("asteroid.png"),
+            "pos": {"x": 0, "y": 0},
+            "angle": random.randint(0, 90)
         }
+
         game["asteroids"].append(newAsteroid)
     ship = game["ship"]
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+        elif event.type == pygame.KEYUP:
+            if event.key == pygame.K_SPACE:
+                bullet = {
+                    "rect": pygame.image.load("bullet.png").get_rect(),
+                    "image": pygame.image.load("bullet.png"),
+                    "pos": {"x": game["ship"]["rect"].center[0], "y": game["ship"]["rect"].center[1]},
+                    "angle": ship["angle"],
+                    "fired": True
+                }
+                game["bullet"].append(bullet)
+
     key = pygame.key.get_pressed()
     if key[pygame.K_LEFT]:
         ship["angle"] += 2
     elif key[pygame.K_RIGHT]:
         ship["angle"] -= 2
 
-    elif key[pygame.K_SPACE]:
-        bullet["fired"] = True
-        bullet["angle"] = game["ship"]["angle"]
-        bullet["pos"]["x"] = game["ship"]["rect"].center[0]
-        bullet["pos"]["y"] = game["ship"]["rect"].center[1]
+    for bullet in game["bullet"]:
+        bulletAngle = bullet["angle"]
+        bulletAngle = bulletAngle + 180
+        if bullet["fired"] == True:
 
-    bulletAngle = bullet["angle"]
-    bulletAngle = bulletAngle + 180
-    if bullet["fired"] == True:
-        bullet
-        bullet["pos"]["x"] += 5 * math.sin(bulletAngle * 0.017453)
-        bullet["pos"]["y"] += 5 * math.cos(bulletAngle * 0.017453)
-        if bullet["pos"]["x"] > 700 or bullet["pos"]["x"] < 0:
-            bullet["fired"] == False
-        if bullet["pos"]["y"] > 700 or bullet["pos"]["y"] < 0:
-            bullet["fired"] == False
+            bullet["pos"]["x"] += 5 * math.sin(bulletAngle * 0.017453)
+            bullet["pos"]["y"] += 5 * math.cos(bulletAngle * 0.017453)
+            if bullet["pos"]["x"] > 700 or bullet["pos"]["x"] < 0:
+                bullet["fired"] == False
+            if bullet["pos"]["y"] > 700 or bullet["pos"]["y"] < 0:
+                bullet["fired"] == False
     #draw all the sprites now, so we can use their rects to
     #check for collisions
     draw(game)
@@ -96,23 +102,25 @@ def gameLoop(game):
     font = pygame.font.SysFont("georgia", 36)
     #now check for any collisions
     #shiprect = ship["image"].get_rect()
-    for i in range(len(game["asteroids"])-1, -1, -1):
+    for i in range(len(game["asteroids"]) - 1, -1, -1):
         asteroid = game["asteroids"][i]
         asteroid["pos"]["x"] += 5 * math.sin(asteroid["angle"] * 0.017453)
         asteroid["pos"]["y"] += 5 * math.cos(asteroid["angle"] * 0.017453)
         asteroidRect = asteroid["rect"]
-        if bullet["rect"].colliderect(asteroidRect) and bullet["fired"] == True:
-            game["asteroids"].remove(asteroid)
-            ship["hits"] += 1
-        if asteroidRect.colliderect(shipRect):
-            if asteroid in game["asteroids"]:
+        for bullet in game["bullet"]:
+            if bullet["rect"].colliderect(asteroidRect) and bullet["fired"] == True:
                 game["asteroids"].remove(asteroid)
+                game['bullet'].remove(bullet)
+                ship["hits"] += 1
+            if asteroidRect.colliderect(shipRect):
+                if asteroid in game["asteroids"]:
+                    game["asteroids"].remove(asteroid)
 
-            ship["health"] -= 1
-            if ship["health"] == 0:
-                print("You Died D: you hit " + str(ship["hits"]) + " asteroids")
-                pygame.quit()
-                sys.exit()
+                ship["health"] = ship["health"] - 1
+                if ship["health"] == 0:
+                    print("You Died D: you hit " + str(ship["hits"]) + " asteroids")
+                    pygame.quit()
+                    sys.exit()
 
 
 def draw(game):
@@ -121,11 +129,11 @@ def draw(game):
     backgroundRect = background.get_rect()
 
 
- #   draw_centre_x = ship["position"]["x"] - surface_centre[0]
- #   draw_centre_y = ship["position"]["y"] - surface_centre[1]
- #   draw_centre = draw_centre_x, draw_centre_y
+    #   draw_centre_x = ship["position"]["x"] - surface_centre[0]
+    #   draw_centre_y = ship["position"]["y"] - surface_centre[1]
+    #   draw_centre = draw_centre_x, draw_centre_y
 
-#    screen.blit(rotated_surface, draw_centre)
+    #    screen.blit(rotated_surface, draw_centre)
 
 
     # Rotate the ship image and then draw it
@@ -136,39 +144,38 @@ def draw(game):
     surface_centre = rotated_surface.get_rect().center
     draw_centre_x = ship["position"]["x"] - surface_centre[0]
     draw_centre_y = ship["position"]["y"] - surface_centre[1]
-    shipRect= shipImage.get_rect()
+    shipRect = shipImage.get_rect()
     shipRect = shipRect.move(draw_centre_x - shipRect.right, draw_centre_y - shipRect.top)
     ship["rect"] = shipRect
     screen.blit(background, backgroundRect)
     screen.blit(rotated_surface, shipRect)
 
-
-
     font = pygame.font.SysFont("georgia", 36)
     white = (255, 255, 255)
     black = (0, 0, 0)
     #draw text!
-    text = font.render("you have " + str(ship["health"]) + " lives", True,white,black)
+    text = font.render("you have " + str(ship["health"]) + " lives", True, white, black)
     textrect = text.get_rect()
     screen.blit(text, textrect)
     for asteroid in game["asteroids"]:
         asteroidRect = asteroid["image"].get_rect()
         asteroid_x = asteroid["pos"]["x"]
         asteroid_y = asteroid["pos"]["y"]
-
-
+ 
         asteroid["rect"] = asteroidRect.move(asteroid_x - asteroidRect.right, asteroid_y - asteroidRect.top)
 
         screen.blit(asteroid["image"], asteroid["rect"])
-    bullet = game["bullet"][0]
-    # Draw the bullet, if it has been fired
-    if bullet["fired"] == True:
-        bullet_rect = bullet["image"].get_rect()
-        bulletX = bullet["pos"]["x"]
-        bulletY = bullet["pos"]["y"]
-        bullet["rect"] = bullet_rect.move(bulletX - bullet_rect.right, bulletY - bullet_rect.top)
-        screen.blit(bullet["image"], bullet["rect"])
+    # draw the bullets
+    for bullet in game["bullet"]:
+        if bullet["fired"] == True:
+            bullet_rect = bullet["image"].get_rect()
+            bulletX = bullet["pos"]["x"]
+            bulletY = bullet["pos"]["y"]
+            bullet["rect"] = bullet_rect.move(bulletX - bullet_rect.right, bulletY - bullet_rect.top)
+            screen.blit(bullet["image"], bullet["rect"])
     pygame.display.flip()
+
+
 game = start_state()
 pygame.init()
 init(game)
